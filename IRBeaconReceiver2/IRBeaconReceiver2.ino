64990275a4dc7dc9db25b024ee6ba7444d8a1286
John Fattaruso <fattaruso@ieee.org>
2020-02-04 16:39:53 -0600

    Updated IRBeaconReceiver sketch for the self-contained receiver displaying characters on an LCD

--- IRBeaconReceiver2/IRBeaconReceiver2.ino
+++ IRBeaconReceiver2/IRBeaconReceiver2.ino
@@ -1,22 +1,23 @@
 /* 
 This program interprets a series of IR on-off events as a series of eight binary bits
 as encoded by the NEC remote control protocol. The ASCII character represented
-by the bit seqence is sent to the serial port. The receiver is assumed to give an
+by the bit seqence is displayed on the LCD screen. The receiver is assumed to give an
 inverted output, so the initial event to be captured is a falling edge.
 This code version uses software recognition of IR receiver states, and makes use
 of the micros() Arduino library routine for capturing event timing.
 The IR receiver output should be wired to digital pin 2, which is PORTD bit 2.
-For debugging, the Arduino LED on pin D13 is lit while a character is being
-received, but this code should be removed for use in robot firmware.
 */
 
-#define ledPin 13
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 #define receiverPin 2
 #define EVENT_RISING 1
 #define EVENT_FALLING 2
 #define EVENT_NONE 0
 
-unsigned char IRChar,IRCharBitMask,necState,buffer[8];
+LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7,3,POSITIVE);  // Set the LCD I2C address and parameters
+unsigned char IRChar,IRCharBitMask,necState,buffer[8],row[16];
+int col;
 int buffer_in,buffer_out;
 boolean receiverState = false;
 unsigned long prev_time,cur_time,ticks;
@@ -29,12 +30,17 @@ void setup() {
     else pinMode(i,INPUT);
     i++;
   }
-  Serial.begin(9600);      // setup serial
-  digitalWrite(ledPin,LOW); // reset mode LED
+  lcd.begin(16,2);  // initialize the lcd 
+  lcd.home();       // go to the top line
+  lcd.print("KNW2300 Beacons");  
+  lcd.setCursor(0,1);        // go to the bottom line
+  lcd.blink();      // enable blinking cursor
+  col = 0;
   necState = 0;
   buffer_in = 0;
   buffer_out = 0;
   prev_time = 0;
+  delay(1000);
 }
 
 void loop() {
@@ -56,14 +62,12 @@ void loop() {
     if (necState == 0) {  // Expecting rising edge of leading pulse
       if (event == EVENT_RISING) {
         necState = 1;
-        digitalWrite(ledPin,HIGH);
       }
     }
     else if (necState == 1) {  // Expecting falling edge of leading pulse
       if (event == EVENT_FALLING) {
         if (ticks > 8900L) necState = 2;  // Check for leading pulse > 8.9msec
         else {  // Stray short pulse found, reset NEC state
-          digitalWrite(ledPin,LOW);
           necState = 0;
         }
       }
@@ -76,7 +80,6 @@ void loop() {
           necState = 3;
         }
         else {  // Space too short, reset NEC state to wait for another leading pulse
-          digitalWrite(ledPin,LOW);
           necState = 0;
         }
       }
@@ -85,7 +88,6 @@ void loop() {
       if (event == EVENT_FALLING) {
         if (ticks < 648) necState = 4;  // Check if data pulse width < 648 usec
         else {  // Width too short, reset NEC state to wait for another leading pulse
-          digitalWrite(ledPin,LOW);
           necState = 0;
         }
       }
@@ -99,7 +101,6 @@ void loop() {
         if (IRCharBitMask == 0) {  // Check if eighth bit received and character complete
           buffer[buffer_in] = IRChar;  // Record complete character received in circular output buffer
           buffer_in = (buffer_in + 1) & 0x07;
-          digitalWrite(ledPin,LOW);
           necState = 0;  // Reset NEC state to wait for another leading pulse
         }
         else necState = 3;  // Wait for falling edge of data pulse
@@ -108,9 +109,16 @@ void loop() {
     prev_time = cur_time;
   }
   if (buffer_out != buffer_in) {
-    Serial.write(buffer[buffer_out]);  // Report received characters to serial port
-    Serial.write('\r');
-    Serial.write('\n');
+    lcd.write(buffer[buffer_out]);  // Report received characters to LCD
+    row[col] = buffer[buffer_out];
+    col++;
+    if (col == 16) {
+      lcd.clear();
+      lcd.home();
+      lcd.write(row,16);
+      lcd.setCursor(0,1);        // go to the bottom line
+      col = 0;
+    }
     buffer_out = (buffer_out + 1) & 0x07;
   }
 }
